bcdf4a19a34224eeb7c2d7668a148d47fef04d40
Τρύφων Ρούμπος <tryfonroumbos@gmail.com>
2025-10-05 12:46:38 +0300

    Fixed issue with the formatter

--- lua/plugs/lsp/format.lua
+++ lua/plugs/lsp/format.lua
@@ -5,10 +5,7 @@ return {
 		"zapling/mason-conform.nvim",
 	},
 	config = function()
-		local conform = require("conform")
-		local conform_mason = require("mason-conform")
-
-		conform_mason.setup({
+		require("mason-conform").setup({
 			ensure_installed = {
 				"prettier",
 				"clang-format",
@@ -24,29 +21,29 @@ return {
 				timeout_ms = 500,
 			})
 		end, { desc = "Format file or range (in visual mode)" })
+
+		require("conform").setup({
+			formatters_by_ft = {
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				c = { "clang-format" },
+				cpp = { "clang-format" },
+			},
+
+			format_on_save = {
+				lsp_format = "fallback",
+				lsp_fallback = true,
+				async = false,
+				quiet = false,
+				timeout_ms = 3000,
+			},
+
+			formatters = {
+				injected = { options = { ignore_errors = true } },
+			},
+		})
 	end,
-
-	conform.setup({
-		formatters_by_ft = {
-			json = { "prettier" },
-			yaml = { "prettier" },
-			markdown = { "prettier" },
-			lua = { "stylua" },
-			python = { "isort", "black" },
-			c = { "clang-format" },
-			cpp = { "clang-format" },
-		},
-
-		format_on_save = {
-			lsp_format = "fallback",
-			lsp_fallback = true,
-			async = false,
-			quiet = false,
-			timeout_ms = 3000,
-		},
-
-		formatters = {
-			injected = { options = { ignore_errors = true } },
-		},
-	}),
 }
